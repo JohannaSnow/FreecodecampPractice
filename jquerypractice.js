1b676217630b688a3314da2ae90299dd188c4614
Johanna Snow <johannadsnow@gmail.com>
2017-03-31 20:10:44 -0500

    multiple selectors

--- jquerypractice.js
+++ jquerypractice.js
@@ -148,3 +148,34 @@ $("#target6").addClass("animated fadeOut").
 
 
 ----------------------------------------------------------------------
+
+Target the same element with multiple jQuery Selectors
+-------------------------------------------------------------------------
+Now you know three ways of targeting elements: by type: $("button"), by class: $(".btn"), and by id $("#target1").
+
+Although it is possible to add multiple classes in a single .addClass() call, lets add them to the same element in three separate ways.
+
+Using .addClass(), add only one class at a time to the same element, three different ways:
+
+Add the animated class to all elements with type button.
+
+Add the shake class to all the buttons with class .btn.
+
+Add the btn-primary class to the button with id #target1.
+
+Note
+You should only be targeting one element and adding only one class at a time. Altogether, your three individual selectors will end up adding the three classes shake, animated, and btn-primary to #target1.
+
+
+-----------------------------------------------------------------------------
+
+<script>
+  $(document).ready(function() {
+  $("button").addClass("animated");
+  $(".btn").addClass("shake");
+  $("#target1").addClass("btn-primary");
+  });
+</script>
+
+
+-----------------------------------------------
