5300b31764ad5bf2cd00d12d5abb71ec7b0c8709
Johanna Snow <johannadsnow@gmail.com>
2017-04-02 20:59:40 -0500

    .appendTo

--- jquerypractice.js
+++ jquerypractice.js
@@ -302,3 +302,29 @@ Remove element target4 from the page by using the .remove() function.
 </script>
 
 -------------------------------------------
+
+
+Use appendTo to Move Elements with jQuery
+---------------------------------------------
+
+Now lets try moving elements from one div to another.
+
+jQuery has a function called appendTo() that allows you to select HTML elements and append them to another element.
+
+For example, if we wanted to move target4 from our right well to our left well, we would use:
+
+$("#target4").appendTo("#left-well");
+
+Move your target2 element from your left-well to your right-well.
+
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+
+  });
+</script>
+
+---------------------------------------------
