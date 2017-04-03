9371dce8532f5dfa0194c40751b434c41dd61080
Johanna Snow <johannadsnow@gmail.com>
2017-04-02 21:05:16 -0500

    .parent

--- jquerypractice.js
+++ jquerypractice.js
@@ -358,3 +358,32 @@ Clone your target5 element and append it to your left-well.
 </script>
 
 -----------------------------------------------------
+
+Target the Parent of an Element Using jQuery
+------------------------------------------------
+Every HTML element has a parent element from which it inherits properties.
+
+For example, your jQuery Playground h3 element has the parent element of <div class="container-fluid">, which itself has the parent body.
+
+jQuery has a function called parent() that allows you to access the parent of whichever element you've selected.
+
+Here's an example of how you would use the parent() function if you wanted to give the parent element of the left-well element a background color of blue:
+
+$("#left-well").parent().css("background-color", "blue")
+
+Give the parent of the #target1 element a background-color of red.
+
+
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+
+  });
+</script>
+
+-------------------------------------------------------------------
