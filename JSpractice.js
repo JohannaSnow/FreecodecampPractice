84e01671943edc2b3edfb4449802a9211bc42dfc
Johanna Snow <johannadsnow@gmail.com>
2017-04-16 18:15:12 -0500

    division

--- JSpractice.js
+++ JSpractice.js
@@ -171,6 +171,7 @@ var difference = 45 - 33;
 
 -----------------------------------------
 Multiply Two Numbers with JavaScript
+---------------------------------------
 We can also multiply one number by another.
 
 JavaScript uses the * symbol for multiplication of two numbers.
@@ -183,3 +184,20 @@ Change the 0 so that product will equal 80.
 
 var product = 8 * 10;
 ---------------------------------------------
+
+Divide One Number by Another with JavaScript
+----------------------------------------------------
+We can also divide one number by another.
+
+JavaScript uses the / symbol for division.
+
+Example
+
+myVar = 16 / 2; // assigned 8
+Instructions
+Change the 0 so that the quotient is equal to 2.
+
+
+var quotient = 66 / 33;
+
+--------------------------------------------
