b82967ecd042673d3f558e4687950a2a05ffe65b
Johanna Snow <johannadsnow@gmail.com>
2017-04-16 18:21:57 -0500

    increments

--- JSpractice.js
+++ JSpractice.js
@@ -201,3 +201,22 @@ Change the 0 so that the quotient is equal to 2.
 var quotient = 66 / 33;
 
 --------------------------------------------
+
+Increment a Number with JavaScript
+--------------------------------------------------
+You can easily increment or add one to a variable with the ++ operator.
+
+i++;
+
+is the equivalent of
+
+i = i + 1;
+
+Note
+The entire line becomes i++;, eliminating the need for the equal sign.
+
+Instructions
+Change the code to use the ++ operator on myVar.
+
+Hint
+Learn more about Arithmetic operators - Increment (++).
