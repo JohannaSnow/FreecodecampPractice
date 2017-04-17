00317c4dadeda4dfc311bee71605c6900e8e495e
Johanna Snow <johannadsnow@gmail.com>
2017-04-16 19:06:00 -0500

    augmented subtraction

--- JSpractice.js
+++ JSpractice.js
@@ -347,3 +347,29 @@ a += 12;
 b += 9;
 c += 7;
 ----------------------------------------------------
+
+Compound Assignment With Augmented Subtraction
+-----------------------------------------------------
+Like the += operator, -= subtracts a number from a variable.
+
+myVar = myVar - 5;
+
+will subtract 5 from myVar. This can be rewritten as:
+
+myVar -= 5;
+
+Instructions
+Convert the assignments for a, b, and c to use the -= operator.
+
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+------------------------------------------
