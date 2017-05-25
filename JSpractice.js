aad9529daebaaa58c94cfc4f9a584624775efe0e
Johanna Snow <johannadsnow@gmail.com>
2017-05-25 13:25:35 -0500

    string literal

--- JSpractice.js
+++ JSpractice.js
@@ -452,3 +452,20 @@ function convertToF(celsius) {
 
 // Change the inputs below to test your code
 convertToF(30);
+
+----------------------------------------------
+
+Previously we have used the code
+
+var myName = "your name";
+
+"your name" is called a string literal. It is a string because it is a series of zero or more characters enclosed in single or double quotes.
+
+Instructions
+Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+
+var myFirstName = "Jo(n)";
+var myLastName = "Snow";
+
+-----------------------------------------------------------
